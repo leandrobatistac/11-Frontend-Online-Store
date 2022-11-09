3c1b798af8c76ec56913b64b170ea1ec255c5821
Leandro Batista de Carvalho <leandrobcrv@gmail.com>
2022-11-09 15:34:12 -0300

    Requisito 1

---
+++ src/pages/Home.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+class Home extends React.Component {
+  state = {
+    haveSearch: false,
+  };
+
+  handleChange = ({ target: { name, value } }) => {
+    const tamanho = 0;
+    this.setState({ [name]: value });
+    if (value.length > tamanho) {
+      this.setState({ haveSearch: true });
+    }
+
+    if (value === '') {
+      this.setState({ haveSearch: false });
+    }
+  };
+
+  render() {
+    const { haveSearch } = this.state;
+    return (
+      <div>
+        <form>
+          <input
+            data-testid=""
+            id="search-input"
+            name="search-input"
+            placeholder="Pesquise Aqui"
+            onChange={ this.handleChange }
+          />
+        </form>
+
+        { !haveSearch
+          ? (
+            <span
+              data-testid="home-initial-message"
+            >
+              Digite algum termo de pesquisa ou escolha uma categoria.
+            </span>)
+          : null }
+      </div>
+    );
+  }
+}
+
+export default Home;
